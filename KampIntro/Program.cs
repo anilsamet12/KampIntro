4563a45b1861349dff98d2fd829033b87b4802ab
pc <pc@DESKTOP-V878TDQ>
2022-11-19 16:20:18 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace KampIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayısı = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmişMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if(dolarDun> dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+
+            }
+
+            if(sistemeGirisYapmişMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+
+
+
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
